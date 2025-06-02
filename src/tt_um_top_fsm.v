1a632f8ed6c643f1564c4d1dce36a285c3a8ff0c
LindaPantaleon <lpantaleond1@miumg.edu.gt>
2025-06-01 22:45:39 -0600

    Update tt_um_top_fsm.v

--- src/tt_um_top_fsm.v
+++ src/tt_um_top_fsm.v
@@ -1,42 +1,50 @@
+`default_nettype none
+
 module tt_um_top_fsm (
-    input  wire [7:0] io_in,
-    output wire [7:0] io_out,
+    input  wire [7:0] ui_in,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    output wire [7:0] uo_out,
     input  wire clk,
-    input  wire rst
+    input  wire rst_n,
+    input  wire ena
 );
 
-    wire C1 = io_in[0];
-    wire C2 = io_in[1];
-    wire I  = io_in[2];
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
+    
+    wire C1 = ui_in[0];
+    wire C2 = ui_in[1];
+    wire I  = ui_in[2];
 
     wire [1:0] Ca;
-    wire A;
 
     fsm my_fsm (
         .clk(clk),
-        .reset(rst),
+        .reset(rst_n),
         .C1(C1),
         .C2(C2),
-        .A(A),
         .I(I),
         .Ca(Ca)
     );
 
-    assign io_out[1:0] = Ca;
-    assign io_out[7:2] = 6'b0;
+    assign uo_out[1:0] = Ca;
+    assign uo_out[7:2] = 6'b0;
 
 endmodule
 
 
 module fsm(
     input wire clk, reset,
-    input wire C1, C2, A, I,
+    input wire C1, C2, I,
     output wire [1:0] Ca
 );
     wire [1:0] P;
+    wire A_internal;
 
-    fsm_moore moore_fsm (.clk(clk), .reset(reset), .C1(C1), .C2(C2), .A(A), .P(P));
-    fsm_mealy mealy_fsm (.clk(clk), .reset(reset), .I(I), .P(P), .A(A), .Ca(Ca));
+    fsm_moore moore_fsm (.clk(clk), .reset(reset), .C1(C1), .C2(C2), .A(A_internal), .P(P));
+    fsm_mealy mealy_fsm (.clk(clk), .reset(reset), .I(I), .P(P), .A(A_internal), .Ca(Ca));
 endmodule
 
 
